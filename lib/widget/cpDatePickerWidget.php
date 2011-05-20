9c460c72de1e3d426c6fe42da19308ae2ca1852b
coopers <coopers@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-05-20 03:34:11 -0500

    Merge branch 'master' into svn
    git-svn-id: http://svn.symfony-project.com/plugins/cpDatePickerPlugin/trunk@32546 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/widget/cpDatePickerWidget.php
@@ -0,0 +1,70 @@
+<?php
+class cpDatePickerWidget extends sfWidgetFormInput {
+
+  /**
+   * Constructor.
+   *
+   * Available options:
+   *
+   *  * type: The widget type (text by default)
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array()) {
+    $this->addRequiredOption('culture');
+    $this->addOption('dateFormat', 'yyyy.mm.dd');
+    $this->addOption('startDate', '1901-01-01');
+    $this->addOption('endDate');
+    $this->addOption('clickInput', true);
+    $this->addOption('template', <<<EOF
+%widget%
+<script type="text/javascript">
+  jQuery(document).ready(function() {
+    Date.format = '%date_format%';
+    jQuery('#%id%').datePicker({
+      startDate: '%start_date%',
+      endDate: '%end_date%',
+      clickInput: %click_input%
+    });
+  });
+</script>    
+EOF
+);
+  }
+
+  /**
+   * @param  string $name        The element name
+   * @param  string $value       The value displayed in this widget
+   * @param  array  $attributes  An array of HTML attributes to be merged with the default HTML attributes
+   * @param  array  $errors      An array of errors for the field
+   *
+   * @return string An HTML tag string
+   *
+   * @see sfWidgetForm
+   */
+  public function render($name, $value = null, $attributes = array(), $errors = array()) {
+    $id = $this->generateId($name);
+
+    return strtr($this->getOption('template'), array(
+             '%widget%' => parent::render($name, $value, $attributes, $errors),
+             '%id%' => $id,
+             '%date_format%' => $this->getOption('dateFormat'),
+             '%start_date%' => $this->getOption('startDate'),
+             '%end_date%' => $this->getOption('endDate'),
+             '%click_input%' => $this->getOption('clickInput')
+            ));
+  }
+  
+  public function getJavascripts() {
+    return array('/cpDatePickerWidgetPlugin/js/date.js',
+                 '/cpDatePickerWidgetPlugin/js/date_' . $this->getOption('culture') . '.js',
+                 '/cpDatePickerWidgetPlugin/js/jquery.datePicker.js');
+  }
+  
+  public function getStylesheets() {
+    return array('/cpDatePickerWidgetPlugin/css/datePicker.css' => 'all');
+  }
+}
---
+++ web/js/date_en.js
@@ -0,0 +1,13 @@
+Date.dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+Date.abbrDayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+Date.monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+Date.abbrMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+$.dpText = {
+    TEXT_PREV_YEAR   : 'Previous year',
+    TEXT_PREV_MONTH  : 'Previous month',
+    TEXT_NEXT_YEAR   : 'Next year',
+    TEXT_NEXT_MONTH  : 'Next month',
+    TEXT_CLOSE       : 'Close',
+    TEXT_CHOOSE_DATE : 'Choose date',
+    HEADER_FORMAT    : 'mmmm yyyy'
+  }
